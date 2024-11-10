7624d82d0dac8eca3a2d2fbd485683e4b3f5521d
Kim Ji Eun <{>
2024-11-10 11:30:19 +0900

    ex2

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 // TODO: props로 받은 $active 에 따라 배경색이 blue 또는 gray가 되도록 해보세요.
 const StyledBox = styled.div`
   width: 100px;
   height: 100px;
-  background-color: "gray";
+  background-color: ${(props) => (props.$active ? 'blue' : 'gray')};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -14,8 +14,8 @@ const StyledBox = styled.div`
   font-size: 20px;
 `;
 
-function Box({ active, onClick }) {
-  return <StyledBox $active={active} onClick={onClick} />;
+function Box({ $active, onClick }) {
+  return <StyledBox $active={$active} onClick={onClick} />;
 }
 
 export default Box;
